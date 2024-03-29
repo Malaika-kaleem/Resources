0b53cbf6630096bae9c43a1214ebe9f10262c6f3
PMYLS <malaika.kaleem4@gmail.com>
2024-03-29 23:06:54 +0500

    commited

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       }
+}
